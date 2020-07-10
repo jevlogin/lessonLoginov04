298538416538ced7d3de80597fd4d3bbc9df7907
jevlogin <jev.login@mail.ru>
2020-07-10 20:21:22 +0300

    вынес класс двумерный массив

---
+++ 04les06/MyArrayTwoDim.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+
+/**
+ * 
+ * Задача 4. Класс «Мой двумерный массив» 
+ * Разработать класс для работы с двумерным массивом. 
+ * Сделать методы поиска среднего значения, максимального элемента 
+ * массива, минимального элемента массива, 
+ * подсчета количества положительных элементов массива,
+ * вывода массива на экран и метод, возвращающий массив в виде строки.
+ * 
+ * */
+
+namespace _04les06
+{
+    class MyArrayTwoDim
+    {
+        int[,] a;
+        public MyArrayTwoDim(int n, int el)
+        {
+            a = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    a[i, j] = el;
+                }
+            }
+        }
+        public MyArrayTwoDim(int n, int min, int max)
+        {
+            a = new int[n, n];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    a[i, j] = rnd.Next(min, max);
+                }
+            }
+        }
+        public int Min
+        {
+            get
+            {
+                int min = a[0, 0];
+                for (int i = 0; i < a.GetLength(0); i++)
+                {
+                    for (int j = 0; j < a.GetLength(1); j++)
+                    {
+                        if (a[i, j] < min)
+                        {
+                            min = a[i, j];
+                        }
+                    }
+                }
+                return min;
+            }
+        }
+        public int Max
+        {
+            get
+            {
+                int max = a[0, 0];
+                for (int i = 0; i < a.GetLength(0); i++)
+                {
+                    for (int j = 0; j < a.GetLength(1); j++)
+                    {
+                        if (a[i, j] > max)
+                        {
+                            max = a[i, j];
+                        }
+                    }
+                }
+                return max;
+            }
+        }
+        public int CountPositive
+        {
+            get
+            {
+                int count = 0;
+                for (int i = 0; i < a.GetLength(0); i++)
+                {
+                    for (int j = 0; j < a.GetLength(0); j++)
+                    {
+                        if (a[i, j] > 0) count++;
+                        {
+
+                        }
+                    }
+                }
+                return count;
+            }
+        }
+        public double Average
+        {
+            get
+            {
+                double sum = 0;
+                for (int i = 0; i < a.GetLength(0); i++)
+                {
+                    for (int j = 0; j < a.GetLength(1); j++)
+                    {
+                        sum += a[i, j];
+                    }
+                }
+                return sum / a.GetLength(0) / a.GetLength(1);
+            }
+        }
+        public override string ToString()
+        {
+            string s = string.Empty;
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    s += $"{a[i, j],4}";
+                }
+                s += "\n";
+            }
+            return s;
+        }
+    }
+}
