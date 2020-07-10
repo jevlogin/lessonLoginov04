a0210605fcbec1252774d95c19d3b94dec89ef67
jevlogin <jev.login@mail.ru>
2020-07-10 12:18:39 +0300

    2-ый массив

--- 04les06/Program.cs
+++ 04les06/Program.cs
@@ -4,8 +4,101 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+/**
+ * 
+ * Задача 4. Класс «Мой двумерный массив» 
+ * Разработать класс для работы с двумерным массивом. 
+ * Сделать методы поиска среднего значения, максимального элемента 
+ * массива, минимального элемента массива, 
+ * подсчета количества положительных элементов массива,
+ * вывода массива на экран и метод, возвращающий массив в виде строки.
+ * 
+ * */
+
 namespace _04les06
 {
+    class MyArrayTwoDim
+    {
+        int[,] a;
+        public MyArrayTwoDim(int n, int el)
+        {
+            a = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    a[i, j] = el;
+                }
+            }
+        }
+        public MyArrayTwoDim(int n, int min, int max)
+        {
+            a = new int[n, n];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    a[i, j] = rnd.Next(min, max);
+                }
+            }
+        }
+        public int Min
+        {
+            get
+            {
+                int min = a[0, 0];
+                for (int i = 0; i < a.GetLength(0); i++)
+                {
+                    for (int j = 0; j < a.GetLength(1); j++)
+                    {
+                        if (a[i, j] < min)
+                        {
+                            min = a[i, j];
+                        }
+                    }
+                }
+                return min;
+            }
+        }
+        public int Max
+        {
+            get
+            {
+                int max = a[0, 0];
+                for (int i = 0; i < a.GetLength(0); i++)
+                {
+                    for (int j = 0; j < a.GetLength(1); j++)
+                    {
+                        if (a[i, j] > max)
+                        {
+                            max = a[i, j];
+                        }
+                    }
+                }
+                return max;
+            }
+        }
+        public int CountPositive
+        {
+            get
+            {
+                int count = 0;
+                for (int i = 0; i < a.GetLength(0); i++)
+                {
+                    for (int j = 0; j < a.GetLength(0); j++)
+                    {
+                        if (a[i, j] > 0) count++;
+                        {
+
+                        }
+                    }
+                }
+                return count;
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
