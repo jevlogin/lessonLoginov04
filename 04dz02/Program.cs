42902f09fab404519e0329581c6ca26bd2abbf34
jevlogin <jev.login@mail.ru>
2020-07-11 11:37:07 +0300

    Реализуйте задачу 1 в виде статического класса StaticClass; а) Класс должен содержать статический метод, который принимает на вход массив и решает задачу 1;

---
+++ 04dz02/Program.cs
@@ -0,0 +1,24 @@
+﻿using _04dz01;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04dz02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyArray array = new MyArray(20, 0, 10);
+            Console.WriteLine($"{array}");
+
+            int count = StaticClass.CountPairsDivisibleByThree(array.GetArray);
+
+            Console.WriteLine($"Количество пар элементов массива,\nв которых только одно число делится на 3 равно: {count}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 04dz02/StaticClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04dz02
+{
+    static class StaticClass
+    {
+        public static int CountPairsDivisibleByThree(int[] a)
+        {
+            int count = 0;
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                if (a[i] % 3 == 0 && a[i + 1] % 3 == 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+
+}
