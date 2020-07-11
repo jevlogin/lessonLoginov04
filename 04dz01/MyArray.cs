0ab7ac3c0ab52504e3a002248570fb8e3f1fbb3e
jevlogin <jev.login@mail.ru>
2020-07-11 11:14:37 +0300

    Домашнее задание 4 урок. 1 к проверке 04dz01

---
+++ 04dz01/MyArray.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _04dz01
+{
+    class MyArray
+    {
+        private int[] a;
+        private const int MinA = -10_000;
+        private const int MaxA = 10_000;
+
+        Random rnd = new Random();
+
+        public MyArray(int n)
+        {
+            a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = rnd.Next(MinA, MaxA);
+            }
+        }
+
+        public MyArray(int n, int minA, int maxA)
+        {
+            a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = rnd.Next(minA, maxA);
+            }
+        }
+
+        public override string ToString()
+        {
+            string s = string.Empty;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    s += $"{a[i],6}";
+                }
+                else
+                {
+                    s += $"{a[i],6}\n";
+                }
+            }
+            return s;
+        }
+
+        public void Print()
+        {
+            foreach (var el in a)
+            {
+                Console.WriteLine(el);
+            }
+        }
+
+        public int CountPairsDivisibleByThree()
+        {
+            int count = 0;
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                if (a[i] % 3 == 0 && a[i + 1] % 3 == 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ 04dz01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _04dz01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyArray array = new MyArray(20);
+            Console.WriteLine(array);
+            Console.WriteLine($"Количество пар элементов массива,\nв которых только одно число делится на 3 равно: {array.CountPairsDivisibleByThree()}");
+
+            Console.ReadKey();
+        }
+    }
+}
